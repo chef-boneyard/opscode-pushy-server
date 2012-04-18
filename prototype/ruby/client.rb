bbfcb200d851c95696b549434efe7700fb8ca675
Mark Anderson <mark@opscode.com>
2012-04-18 11:54:28 -0700

    Check in testing code for minimal possible client/server. hb's each way work, but not simultaneously.

---
+++ prototype/ruby/client.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/ruby
+
+#
+# Shamelessly stolen from the zmq examples
+#
+
+
+require 'rubygems'
+require 'ffi-rzmq'
+require 'pp'
+require 'uuidtools'
+
+server =  "tcp://localhost"
+sub_port = 5556
+hb_port  = 5557
+
+# 
+context = ZMQ::Context.new(1)
+
+# Connect to server
+subscriber = context.socket(ZMQ::SUB)
+subscriber.connect("#{server}:#{sub_port}")
+# Disable filtering
+subscriber.setsockopt(ZMQ::SUBSCRIBE, '')
+
+#
+# 
+#
+
+puts "Starting"
+
+def listen_blocking(subscriber) 
+  while true
+    subscriber_msg= ''
+    subscriber.recv_string(subscriber_msg)
+    # process update
+    puts "ping: #{subscriber_msg}"
+  end
+end
+
+def listen_nonblocking(subscriber) 
+  while true
+    subscriber_msg= ''
+    if subscriber.recv_string(subscriber_msg, ZMQ::NOBLOCK) && !subscriber_msg.empty?
+      puts "ping: #{subscriber_msg}"
+    end
+    sleep 0.001
+  end
+end
+
+def heartbeat_server(context, server, port)
+  pusher = context.socket(ZMQ::PUSH)
+  pusher.connect("#{server}:#{port}")
+  sequence = 0
+  host = (`hostname`).chomp
+  id   = UUIDTools::UUID.random_create
+  while true
+    time = Time.now()
+    update = "%08d %s %s %s" % [sequence, host, id, time]
+    puts update
+    pusher.send_string(update)
+    sequence+=1
+    sleep 1
+  end
+end
+
+#listen_blocking(subscriber)
+heartbeat_server(context, server, hb_port)
+
+
---
+++ prototype/ruby/server.rb
@@ -0,0 +1,45 @@
+#
+# Binds PUB socket to tcp://*:5556
+#
+
+require 'rubygems'
+require 'ffi-rzmq'
+
+
+listen_if = "tcp://*"
+pub_port  = 5556
+hb_port   = 5557
+
+context = ZMQ::Context.new(1) # one io thread
+
+def server_ping(context, listen_if, port)
+  publisher = context.socket(ZMQ::PUB)
+  publisher.bind("#{listen_if}:#{port}")
+  
+  sequence = 0
+  server = (`hostname`).chomp!
+  while true
+    time = Time.now()
+    update = "%08d %s %s" % [sequence, server, time]
+    puts update
+    publisher.send_string(update)
+    sequence+=1
+    sleep 1
+  end
+end
+
+def hb_collect(context, listen_if, port)
+  hb_pull = context.socket(ZMQ::PULL)
+  hb_pull.bind("#{listen_if}:#{port}")
+
+  while true
+    msg= ''
+    hb_pull.recv_string(msg)
+    # process update
+    puts "hb: #{msg}"
+  end
+end
+
+
+#server_ping(context, listen_if, pub_port)
+hb_collect(context, listen_if, hb_port)
