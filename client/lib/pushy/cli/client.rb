9bd89f3c4bbad9289807d09937e3db45345b6421
mpeck <matthew@opscode.com>
2012-05-03 16:22:09 -0700

    added logging

--- client/lib/pushy/cli/client.rb
+++ client/lib/pushy/cli/client.rb
@@ -9,14 +9,45 @@ module Pushy
       option :config_file,
         :short => "-c CONFIG",
         :long => "--config CONFIG",
-        :default => '',
+        :default => "config.rb",
         :description => "The configuration file to use"
 
-      def initialize
-        super
+      option :port,
+        :short => "-p PORT",
+        :long => "--port PORT",
+        :default => "5556",
+        :description => "use PORT (default: 5555)"
 
+      option :address,
+        :short => "-a HOST",
+        :long => "--address HOST",
+        :default => "0.0.0.0",
+        :description => "bind to HOST address (default: 0.0.0.0)"
+
+      option :verbose,
+        :short => "-v",
+        :long => "--verbose",
+        :boolean => true,
+        :description => "Be verbose"
+
+      option :help,
+        :short => "-h",
+        :long => "--help",
+        :description => "Show this message",
+        :on => :tail,
+        :boolean => true,
+        :show_options => true,
+        :exit => 0
+
+      option :log_level,
+        :short => "-l LEVEL",
+        :long  => "--log_level LEVEL",
+        :description => "Set the log level (debug, info, warn, error, fatal) (default: warn)",
+        :default => :error,
+        :proc => Proc.new { |l| l.to_sym }
+
+      def setup
         trap("TERM") do
-
         end
 
         trap("INT") do
@@ -31,21 +62,29 @@ module Pushy
           reconfigure
         end
 
-        run
+        self.parse_options
+
+        # set log level
+        Pushy::Log.level = config[:verbose] ? :debug : config[:log_level]
       end
 
       def run
         reconfigure
-        client = Pushy::Client.new
+        client = Pushy::Client.new(config)
         client.start
       end
 
       def reconfigure
-        return if config[:config_file].empty?
+        unless ::File.exists? config[:config_file]
+          Pushy::Log.warn "No config file. Using command line options."
+          return
+        end
 
         ::File::open(config[:config_file]) do |f|
           Chef::Config.from_file(f.path)
         end
+
+        true
       end
 
     end
