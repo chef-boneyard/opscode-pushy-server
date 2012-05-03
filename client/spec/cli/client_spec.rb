03e354f9ec131a30925cd0844d66e368526ac958
mpeck <matthew@opscode.com>
2012-05-03 16:32:12 -0700

    [oc-138] PUSHY::CLI::Client#reconfigure without config file should be nil

--- client/spec/cli/client_spec.rb
+++ client/spec/cli/client_spec.rb
@@ -7,6 +7,8 @@ describe Pushy::CLI::Client do
     let(:cli_client) { Pushy::CLI::Client.new }
 
     context "with config file" do
+      let(:given) { cli_client.config[:config_file] = config_filename }
+
       let(:config_file) do
         Tempfile.new('config').tap do |file|
           file.write rand(1000).to_s
@@ -16,12 +18,13 @@ describe Pushy::CLI::Client do
 
       let(:config_filename) { config_file.path }
 
-      let(:given) { cli_client.config[:config_file] = config_filename }
-
       it { should be_true }
     end
 
     context "without config file" do
+      let(:given) { nil }
+
+      it { should be_nil }
     end
   end
 end
