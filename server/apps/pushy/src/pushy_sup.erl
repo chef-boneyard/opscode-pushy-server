6b089ee80996754235909d606dc7c7aad77ece24
Mark Anderson <mark@opscode.com>
2012-05-29 17:23:20 -0700

    Add web machine start to supervisior.

--- server/apps/pushy/src/pushy_sup.erl
+++ server/apps/pushy/src/pushy_sup.erl
@@ -28,8 +28,32 @@ start_link(Ctx) ->
 %% ===================================================================
 
 init([Ctx]) ->
+    Ip = case os:getenv("WEBMACHINE_IP") of false -> "0.0.0.0"; Any -> Any end,
+    {ok, Dispatch} = file:consult(filename:join(
+                                    [code:priv_dir(pushy), "dispatch.conf"])),
+
+    %% Set up trace dir specific stuff
+    {_,_,[{trace_dir, TraceDir}]} = lists:keyfind(["dev", "wmtrace", '*'], 1, Dispatch),
+    ok = filelib:ensure_dir(string:join([TraceDir,"trace"], "/")),
+
+    Port = case application:get_env(pushy, api_port) of
+               {ok, P} -> P;
+               undefined ->
+                   fast_log:todo() %% FIXME
+           end,
+    LogDir = case application:get_env(pushy, log_dir) of
+                 {ok, LD} -> LD;
+                 undefined -> "priv/log"
+             end,
+    WebMachineConfig = [
+                        {ip, Ip},
+                        {port, Port},
+                        {log_dir, LogDir},
+                        {dispatch, Dispatch},
+                        {enable_perf_logger, true}],
     {ok, {{one_for_one, 3, 60},
           [?SUP(pushy_node_state_sup, []),
            ?WORKER(chef_keyring, []),
            ?WORKER(pushy_heartbeat_generator, [Ctx]),
-           ?WORKER(pushy_node_status_tracker, [Ctx])]}}.
+           ?WORKER(pushy_node_status_tracker, [Ctx]),
+           ?WORKER(webmachine_mochiweb, [WebMachineConfig]) ]}}. %% FIXME start or start_link here?
