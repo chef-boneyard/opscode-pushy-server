f064f7a25ef444ac5b1670d84e02e791503c4385
Mark Anderson <mark@opscode.com>
2012-06-03 13:52:14 -0700

    Placeholder for command switch code.

---
+++ server/apps/pushy/src/pushy_command_switch.erl
@@ -0,0 +1,7 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
+%% ex: ts=4 sw=4 et
+%%% @author Mark Anderson <mark@opscode.com>
+%%% @copyright Copyright 2012-2012 Opscode Inc.
+%%% @doc
+%%% Mux-demux for commands to clients and responses from clients
+%%% @end
