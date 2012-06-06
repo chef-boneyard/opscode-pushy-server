050911272eaddfc230fb1480e948ca7841503ea0
Matthew Peck <matthew@opscode.com>
2012-06-06 16:15:39 -0400

    factored out Utils.valid? and Utils.parse_json

--- client/lib/pushy/client/handler.rb
+++ client/lib/pushy/client/handler.rb
@@ -15,7 +15,7 @@ module Pushy
 
         if valid?(parts)
           monitor.checkin!
-          parse_json(parts[1].copy_out_string)
+          Utils.parse_json(parts[1].copy_out_string)
         end
 
       end
@@ -23,21 +23,7 @@ module Pushy
       private
 
       def valid?(parts)
-        auth = parts[0].copy_out_string.split(':')[2]
-        body = parts[1].copy_out_string
-
-        decrypted_checksum = @client.server_public_key.public_decrypt(Base64.decode64(auth))
-        hashed_body = Mixlib::Authentication::Digester.hash_string(body)
-
-        decrypted_checksum == hashed_body
-      end
-
-      def parse_json(json)
-        body_hash = Yajl::Parser.new.parse(json)
-
-        body_hash.keys.each do |key|
-          puts "#{key}: #{body_hash[key]}"
-        end
+        Utils.valid?(parts, @client.server_public_key)
       end
 
     end
@@ -51,5 +37,27 @@ module Pushy
 
     end
 
+    module Utils
+
+      def self.valid?(parts, server_public_key)
+        auth = parts[0].copy_out_string.split(':')[2]
+        body = parts[1].copy_out_string
+
+        decrypted_checksum = server_public_key.public_decrypt(Base64.decode64(auth))
+        hashed_body = Mixlib::Authentication::Digester.hash_string(body)
+
+        decrypted_checksum == hashed_body
+      end
+
+      def self.parse_json(json)
+        body_hash = Yajl::Parser.new.parse(json)
+
+        body_hash.keys.each do |key|
+          puts "#{key}: #{body_hash[key]}"
+        end
+      end
+
+    end
   end
 end
+
