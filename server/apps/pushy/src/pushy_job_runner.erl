e3c76c535e92d956c9e89abdd0d342569d5610e2
Seth Chisamore <schisamo@opscode.com>
2012-06-11 22:12:29 -0400

    add quorum to jobs and level up documentation in job_runner

--- server/apps/pushy/src/pushy_job_runner.erl
+++ server/apps/pushy/src/pushy_job_runner.erl
@@ -14,23 +14,22 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/1]).
+-export([start_link/1,
+         node_command_event/3,
+         node_heartbeat_event/3]).
+
+%% ------------------------------------------------------------------
+%% State Function Exports
+%% ------------------------------------------------------------------
 
 -export([load_from_db/2,
-         register_process/2]).
+         register_process/2,
+         start_vote/2]).
 
--define(NO_JOB, {error, no_job}).
--define(JOB_NODE_EVENT(Event), Event(JobId, NodeName, Type) -> case catch gproc:send({n,l,JobId}, {Event, NodeName, Type}) of
-                                              {'EXIT', _} -> ?NO_JOB;
-                                              _ -> ok
-                                          end).
+%% ------------------------------------------------------------------
+%% Event Handler Function Exports
+%% ------------------------------------------------------------------
 
--record(state, {job_id,
-                job,
-                acked_nodes=[],
-                nacked_nodes=[]}).
-
--include_lib("pushy_sql.hrl").
 
 %% ------------------------------------------------------------------
 %% gen_fsm Function Exports
@@ -40,10 +39,26 @@
          handle_event/3,
          handle_sync_event/4,
          handle_info/3,
-         node_command_event/3,
-         node_heartbeat_event/3,
          terminate/3,
          code_change/4]).
+
+% time in milliseconds to wait for all nodes to ACK/NACK if they can execute the
+% job's command.  If quorum is met we run it, otherwise the job is marked failed.
+-define(JOB_QUORUM_THRESHOLD_TIMEOUT, 10000). % 10 seconds
+-define(NO_JOB, {error, no_job}).
+-define(JOB_NODE_EVENT(Event), Event(JobId, NodeName, Type) -> case catch gproc:send({n,l,JobId}, {Event, NodeName, Type}) of
+                                              {'EXIT', _} -> ?NO_JOB;
+                                              _ -> ok
+                                          end).
+
+-record(state, {job_id,
+                job,
+                acked_nodes=[],
+                nacked_nodes=[],
+                qtref,
+                quorum}).
+
+-include_lib("pushy_sql.hrl").
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -52,6 +67,42 @@
 start_link(JobId) ->
     gen_fsm:start_link({local, ?SERVER}, ?MODULE, [JobId], []).
 
+?JOB_NODE_EVENT(node_heartbeat_event).
+?JOB_NODE_EVENT(node_command_event).
+
+%% ------------------------------------------------------------------
+%% State Function Definitions
+%% ------------------------------------------------------------------
+
+load_from_db(timeout, #state{job_id=JobId}=State) ->
+    case pushy_sql:fetch_job(JobId) of
+        {ok, #pushy_job{job_nodes=JobNodes}=Job} ->
+            %% TODO - eventually quorum will be configurable, for now we'll keep
+            %% it simple and say all nodes must ACK.
+            Quorum = lists:flatlength(JobNodes),
+            {next_state, register_process, State#state{quorum=Quorum, job=Job}, 0};
+        {ok, not_found} ->
+            error_logger:error_msg("Failed to find job ~p for ~p~n", [JobId, self()]),
+            {stop, {error, not_found}, State}
+    end.
+
+register_process(timeout, #state{job_id=JobId}=State) ->
+    case gproc:reg({n, l, JobId}) of
+        true ->
+            {next_state, start_vote, State, 0};
+        false ->
+            error_logger:error_msg("Failed to register job tracker process ~p for ~p~n", [JobId, self()]),
+            {stop, {error, not_registered}, State}
+    end.
+
+start_vote(timeout, #state{job_id=JobId}=State) ->
+    error_logger:info_msg("Commencing vote for job ~s~n", [JobId]),
+    {next_state, tally_vote, start_voting(State)}.
+
+%% ------------------------------------------------------------------
+%% Event Handler Function Definitions
+%% ------------------------------------------------------------------
+
 %% ------------------------------------------------------------------
 %% gen_fsm Function Definitions
 %% ------------------------------------------------------------------
@@ -59,62 +110,42 @@ start_link(JobId) ->
 init([JobId]) ->
     {ok, load_from_db, #state{job_id=JobId}, 0}.
 
-load_from_db(timeout, #state{job_id=JobId}=State) ->
-    case pushy_sql:fetch_job(JobId) of
-        {ok, #pushy_job{}=Job} ->
-            {next_state, register_process, State#state{job=Job}, 0};
-        {ok, not_found} ->
-            error_logger:error_msg("Failed to find job ~p for ~p~n", [JobId, self()]),
-            {stop, not_found, State}
-    end.
-
-register_process(timeout, #state{job_id=JobId, job=#pushy_job{duration=Duration}}=State) ->
-    case gproc:reg({n, l, JobId}) of
-        true ->
-            %% TODO compute timeout based on existing created_at
-            erlang:send_after(Duration*1000, self(), expired),
-            error_logger:info_msg("Beginning execution of job ~s~n", [JobId]),
-            {next_state, executing, execute_job(State)};
-        false ->
-            error_logger:error_msg("Failed to register job tracker process ~p for ~p~n", [JobId, self()]),
-            {stop, shutdown, State}
-    end.
-
-?JOB_NODE_EVENT(node_heartbeat_event).
-?JOB_NODE_EVENT(node_command_event).
-
-handle_event(complete, _StateName, #state{job_id=JobId}=State) ->
-    error_logger:info_msg("Job [~p] complete.~n", [JobId]),
-    {stop, job_complete, save_job_status(complete, State)};
 handle_event(_Event, StateName, State) ->
     {next_state, StateName, State}.
 
 handle_sync_event(_Event, _From, StateName, State) ->
     {reply, ok, StateName, State}.
 
+%% Timeout message handlers..triggered by a erlang:send_after/3
+%% ANYTHING -> expired
+handle_info({expired, Reason}, _StateName, #state{job_id=JobId}=State) ->
+    error_logger:info_msg("Job [~p] expired for reason: ~p.~n", [JobId,Reason]),
+    %% TODO - instruct all nodes to ABORT
+    {stop, {error, job_expired}, save_job_status(expired, State)};
+%% ANYTHING -> failed
+handle_info({failed, Reason}, _StateName, #state{job_id=JobId}=State) ->
+    error_logger:info_msg("Job [~p] failed for reason: ~p.~n", [JobId,Reason]),
+    %% TODO - instruct all nodes to ABORT
+    {stop, {error, job_failed}, save_job_status(failed, State)};
 
-handle_info(expired, _StateName, #state{job_id=JobId}=State) ->
-    error_logger:info_msg("Job [~p] expired.~n", [JobId]),
-    {stop, job_expired, save_job_status(expired, State)};
+%% out of band messages (mostly other process updating us of our nodes' progress)
 handle_info({node_command_event, NodeName, ack}, StateName,
                 #state{job_id=JobId, acked_nodes=AckedNodes}=State) ->
     error_logger:info_msg("Job [~p] ACK received for node [~p].~n", [JobId, NodeName]),
-    {next_state, StateName,
-        execute_start_check(State#state{acked_nodes = [NodeName|AckedNodes]})};
+    execute_start_check(StateName, State#state{acked_nodes = [NodeName|AckedNodes]});
 handle_info({node_command_event, NodeName, nack}, StateName,
                 #state{job_id=JobId, nacked_nodes=NackedNodes}=State) ->
     error_logger:info_msg("Job [~p] NACK received for node [~p].~n", [JobId, NodeName]),
-    {next_state, StateName,
-        execute_start_check(State#state{nacked_nodes = [NodeName|NackedNodes]})};
+    execute_start_check(StateName, State#state{nacked_nodes = [NodeName|NackedNodes]});
 handle_info({node_command_event, NodeName, CommandEvent}, StateName, State) ->
     Status = event_to_job_status(CommandEvent),
     error_logger:info_msg("Node [~p] job status changed [~p]~n", [NodeName, Status]),
-    {next_state, StateName, job_complete_check(save_job_node_status(Status, NodeName, State))};
+    job_complete_check(StateName, save_job_node_status(Status, NodeName, State));
 handle_info({node_heartbeat_event, NodeName, down}, StateName, State) ->
     error_logger:info_msg("Node [~p] job status changed [failed]~n", [NodeName]),
-    {next_state, StateName, job_complete_check(save_job_node_status(failed, NodeName, State))};
+    job_complete_check(StateName, save_job_node_status(failed, NodeName, State));
 handle_info(Info, StateName, State) ->
-    error_logger:info_msg("JOB STATUS CATCH ALL: Info->~p StateName->~p~n", [Info,StateName]),
+    error_logger:info_msg("Job runner catch-all: Message -> ~p Current State -> ~p~n", [Info,StateName]),
     {next_state, StateName, State}.
 
 terminate(_Reason, _StateName, _State) ->
@@ -134,12 +165,7 @@ event_to_job_status(started) ->
 event_to_job_status(finished) ->
     complete.
 
-execute_job(#state{job=#pushy_job{job_nodes=JobNodes}=Job}=State) ->
-    NodeNames = [ JobNode#pushy_job_node.node_name || JobNode <- JobNodes ],
-    %[ pushy_node_state:start_watching(NodeName) || NodeName <- NodeNames],
-    pushy_command_switch:send_multi_command(?POC_ORG_NAME, NodeNames, create_message({command, Job})),
-    save_job_status(executing, State).
-
+%% creates a well-formed JSON message for the pushy_command_switch to send
 create_message(Data) when is_list(Data) ->
     Host = pushy_util:get_env(pushy, server_name, fun is_list/1),
     jiffy:encode({[{server, list_to_binary(Host)} | Data]});
@@ -151,28 +177,76 @@ create_message({execute, #pushy_job{id=JobId}}) ->
     create_message([{type, <<"job_execute">>},
                     {job_id, JobId}]).
 
-status_complete(#pushy_job_node{status=Status}) ->
-    lists:member(Status, ?COMPLETE_STATUS).
+%% Sends the initial job_command message out to nodes who reply with an ACK/NACK
+%% depending on if they are available to execute the Job's command.  Two timeouts
+%% are created, one for over all job execution expiration and one which gives nodes
+%% time to ACK/NACK before final quorum check is performed.
+start_voting(#state{job=#pushy_job{duration=Duration, job_nodes=JobNodes}=Job}=State) ->
+    NodeNames = [ JobNode#pushy_job_node.node_name || JobNode <- JobNodes ],
+    %% total job expiration timeout
+    %% TODO compute timeout based on existing created_at
+    erlang:send_after(Duration*1000, self(), {expired, execution_duration_up}),
+    %% quorum timeout - we can only wait a set time for ACK/NACKs from this Job's nodes
+    QTRef = erlang:send_after(?JOB_QUORUM_THRESHOLD_TIMEOUT, self(), {failed, no_quorum}),
+    %% subscribe to real-time node status updates via the heartbeat channel
+    % [ pushy_node_state:start_watching(NodeName) || NodeName <- NodeNames],
+    %% Send the voting message out; we expect an ACK/NACK back from our nodes.
+    %% The quorum timeout should cover the case where some nodes don't vote (how rude).
+    pushy_command_switch:send_multi_command(?POC_ORG_NAME, NodeNames, create_message({command, Job})),
+    save_job_status(voting, State#state{qtref=QTRef}).
 
-job_complete_check(#state{job=#pushy_job{job_nodes=JobNodes}}=State) ->
-    case lists:all(fun status_complete/1, JobNodes) of
-        true ->
-            gen_fsm:send_all_state_event(self(), complete),
-            State;
-        false ->
-            State
-    end.
+%% Sends the job_execute message to all nodes who have ACKED
+start_executing(#state{acked_nodes=AckedNodes, job=Job}=State) ->
+    %% send a job_execute message to all acked nodes
+    pushy_command_switch:send_multi_command(?POC_ORG_NAME,
+                                            AckedNodes,
+                                            create_message({execute, Job})),
+    save_job_status(executing, State).
 
-execute_start_check(#state{acked_nodes=AckedNodes, nacked_nodes=NackedNodes,
-                        job=#pushy_job{job_nodes=JobNodes}=Job}=State) ->
+%% First checks if all nodes have reported back.  If quorum has been met, the
+%% quorum timeout timer is cancelled and all nodes who have ACKED are instructed
+%% to begin executing the job's command.
+execute_start_check(CurrentState,
+                        #state{job_id=JobId,
+                                acked_nodes=AckedNodes, nacked_nodes=NackedNodes,
+                                quorum=Quorum, qtref=QTRef,
+                                job=#pushy_job{job_nodes=JobNodes}}=State) ->
+    %% first we check if everyone has voted
     case lists:flatlength(JobNodes) == lists:flatlength([AckedNodes|NackedNodes]) of
         true ->
-            pushy_command_switch:send_multi_command(?POC_ORG_NAME, AckedNodes, create_message({execute, Job})),
-            State;
+            %% if so we check if quorum has been met
+            case Quorum == lists:flatlength(AckedNodes) of
+                true ->
+                    %% cancel the quorom wait timeout timer
+                    erlang:cancel_timer(QTRef),
+                    error_logger:info_msg("Beginning execution of Job [~p].~n", [JobId]),
+                    {next_state, execute, start_executing(State)};
+                false ->
+                    {stop, {error, no_quorum}, State}
+                end;
         false ->
-            State
+            {next_state, CurrentState, State}
     end.
 
+%% Checks if all ACKED nodes have completed running the job.  This does not
+%% necessarily mean the job was successful.
+job_complete_check(CurrentState, #state{job_id=JobId,
+                                        job=#pushy_job{job_nodes=JobNodes}}=State) ->
+    %% Check if all nodes have returned a status that is considered complete
+    case lists:all(
+            fun(#pushy_job_node{status=Status}) ->
+                lists:member(Status, ?COMPLETE_STATUS)
+            end,
+            JobNodes) of
+        true ->
+            error_logger:info_msg("Job [~p] complete.~n", [JobId]),
+            %% TODO - mark job status 'error' if any nodes encountered issues
+            {stop, {ok, job_complete}, save_job_status(complete, State)};
+        false ->
+            {next_state, CurrentState, State}
+    end.
+
+%% Persists the overall job's status to the database.
 save_job_status(Status, #state{job=Job}=State) ->
     Job1 = Job#pushy_job{status=Status},
     case pushy_object:update_object(update_job, Job1, ?POC_ACTOR_ID) of
@@ -182,6 +256,7 @@ save_job_status(Status, #state{job=Job}=State) ->
             State#state{job=Job1}
     end.
 
+%% Persists the node's job status to the database.
 save_job_node_status(Status, NodeName, #state{job=#pushy_job{job_nodes=Nodes}=Job}=State) ->
     Fun1 = fun(#pushy_job_node{node_name=Name}) -> Name =:= NodeName end,
     {[Node | _], Rest} = lists:partition(Fun1, Nodes),
