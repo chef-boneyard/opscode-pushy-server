1e801007fa11aabc5724af122683afd9d89bd667
jkeiser <jkeiser@opscode.com>
2012-08-20 17:04:43 -0700

    Speed up tests with twitchier down detection

--- apps/pushy/src/pushy_node_state.erl
+++ apps/pushy/src/pushy_node_state.erl
@@ -48,7 +48,7 @@
 
 -define(DEFAULT_DECAY_INTERVAL, 4).
 -define(DEFAULT_UP_THRESHOLD, 0.5).
--define(DEFAULT_DOWN_THRESHOLD, 0.2).
+-define(DEFAULT_DOWN_THRESHOLD, 0.4).
 
 -record(state, {node_ref              :: node_ref(),
                 heartbeat_interval    :: integer(),
