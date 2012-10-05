0d8f44197fdf7f6f9bd3a06c02e7ad84c02f9e6a
jamesc <james@opscode.com>
2012-10-04 17:21:26 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source :rubygems
+
+omnibus_ruby_local_path = File.expand_path(File.join(File.dirname(__FILE__), "..", "omnibus-ruby"))
+omnibus_software_local_path = File.expand_path(File.join(File.dirname(__FILE__), "..", "omnibus-software"))
+
+#gem "omnibus", :path => omnibus_ruby_local_path
+gem "omnibus", :git => "git://github.com/opscode/omnibus-ruby.git", :branch => 'master'
+
+#gem "omnibus-software", :path => omnibus_software_local_path
+gem "omnibus-software", :git => "git://github.com/opscode/omnibus-software.git", :branch => 'master'
+
+group :development do
+  gem "vagrant", "~> 1.0"
+end
+
---
+++ config/projects/pushy-server.rb
@@ -0,0 +1,48 @@
+#
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "pushy-server"
+
+replaces        "pushy-server"
+install_path    "/opt/pushy-server"
+build_version   Omnibus::BuildVersion.full
+build_iteration "1"
+
+deps = []
+
+# global
+deps << "chef-gem"
+deps << "preparation"
+#deps << "pushy-server-cookbooks"
+#deps << "pushy-server-scripts"
+#deps << "pushy-server-ctl"
+deps << "runit"
+
+deps << "postgresql"
+#deps << "chef_db" # required to migrate the DB.
+
+deps << "opscode-pushy-server"
+#deps << "oc-pushy-pedant"
+
+
+# version manifest file
+deps << "version-manifest"
+
+dependencies deps
+
+exclude "\.git*"
+exclude "bundler\/git"
