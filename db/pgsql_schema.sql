dee3f79b89e97b1b7ed00d95d22f8e5c36f75ef7
Matthew Peck <matthew@opscode.com>
2012-10-26 13:40:08 -0400

    changed encoding back to UTF8, not sure why it changed

--- db/pgsql_schema.sql
+++ db/pgsql_schema.sql
@@ -3,7 +3,7 @@
 --
 
 SET statement_timeout = 0;
-SET client_encoding = 'SQL_ASCII';
+SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
