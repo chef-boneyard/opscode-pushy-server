8b995cb21eb8c99b27b6837dfa99c24c14dce6cb
jamesc <james@opscode.com>
2012-11-14 14:45:06 -0800

    flesh out fetch_org_id

---
+++ apps/pushy/test/pushy_org_tests.erl
@@ -0,0 +1,84 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92-*-
+%% ex: ts=4 sw=4 et
+%% @author James Casey <james@opscode.com>
+%% Copyright 2012 Opscode, Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+
+
+-module(pushy_org_tests).
+
+
+-define(ROOT_URL, <<"https://localhost">>).
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+-define(ORG_ID, <<"aaaaa">>).
+
+-include_lib("eunit/include/eunit.hrl").
+
+setup_env() ->
+    ok = application:set_env(pushy, erchef_root_url, ?ROOT_URL).
+
+make_response_body(OrgId) ->
+    jiffy:encode({[{<<"guid">>, OrgId}]}).
+
+simple_test_() ->
+    %% request data
+    RequestId = <<"1234567890123">>,
+    OrgName = <<"test_org">>,
+    %% response data
+    OrgId = <<"test_org_id">>,
+    {foreach,
+     fun() ->
+             setup_env(),
+             meck:new([ibrowse], [])
+     end,
+     fun(_) ->
+              meck:unload()
+     end,
+    [{"Simple success test",
+      fun() -> meck:expect(ibrowse, send_req,
+                           fun(_Url, _Headers, get) ->
+                                    {ok, "200", [], make_response_body(OrgId)}
+                            end),
+               GotOrgId = pushy_org:fetch_org_id(OrgName, RequestId),
+               ?assertEqual(OrgId, GotOrgId)
+               end},
+     {"404 test",
+      fun() -> meck:expect(ibrowse, send_req,
+                           fun(_Url, _Headers, get) ->
+                               {ok, "404", [], []}
+                           end),
+
+               ?assertEqual(not_found, pushy_org:fetch_org_id(OrgName, RequestId))
+      end},
+     {"500 test",
+      fun() -> meck:expect(ibrowse, send_req,
+                           fun(_Url, _Headers, get) ->
+                               {ok, "500", [], []}
+                           end),
+               ?assertThrow({error, {server_error, {"500", [], []}}}, pushy_org:fetch_org_id(OrgName, RequestId))
+      end},
+     {"403 test",
+      fun() -> meck:expect(ibrowse, send_req,
+                           fun(_Url, _Headers, get) ->
+                               {ok, "403", [], []}
+                           end),
+               ?assertThrow({error, {client_error, {"403",[], []}}}, pushy_org:fetch_org_id(OrgName, RequestId))
+      end}
+    ]}.
