bd7ef7c2cb47582b681aa73fd530f7a2bf849cfe
Mark Anderson <mark@opscode.com>
2012-11-15 19:42:57 -0800

    Fix tests; mostly work

--- apps/pushy/test/pushy_node_state_tests.erl
+++ apps/pushy/test/pushy_node_state_tests.erl
@@ -17,18 +17,38 @@
 
 -define(HB_INTERVAL, 100).
 -define(DECAY_WINDOW, 4). %% 4 is friendly to base 2 float arith
+-define(DOWN_THRESHOLD, 0.25).
 
 -include_lib("eunit/include/eunit.hrl").
 
+-define(ASSERT_UP(Node), ?assertMatch({online, {available, _}}, ?NS:status(Node)) ).
+-define(ASSERT_DOWN(Node), ?assertMatch({online, {unavailable, _}}, ?NS:status(Node)) ).
+
+basic_setup() ->
+    test_util:start_apps(),
+    pushy_node_stats:init(),
+    meck:new(pushy_command_switch, []),
+    meck:expect(pushy_command_switch, send,
+                fun(_NodeRef,  _Message) -> ok end),
+    meck:expect(pushy_command_switch, send_command,
+                fun(_NodeRef,  _Message) -> ok end),
+    application:set_env(pushy, heartbeat_interval, ?HB_INTERVAL),
+    application:set_env(pushy, decay_window, ?DECAY_WINDOW),
+    application:set_env(pushy, down_threshold, ?DOWN_THRESHOLD),
+    ok.
+
+
+basic_cleanup() ->
+    ets:delete(pushy_node_stats),
+    meck:unload(pushy_command_switch).
+
+
+
 init_test_() ->
     {foreach,
-     fun() ->
-             test_util:start_apps(),
-             application:set_env(pushy, heartbeat_interval, ?HB_INTERVAL),
-             application:set_env(pushy, decay_window, ?DECAY_WINDOW),
-             ok
-     end,
+     fun basic_setup/0,
      fun(_) ->
+             basic_cleanup(),
              ok
      end,
      [fun(_) ->
@@ -62,16 +82,12 @@ init_test_() ->
 heartbeat_test_() ->
     {foreach,
      fun() ->
-             test_util:start_apps(),
-             meck:new(pushy_command_switch, []),
-             meck:expect(pushy_command_switch, send_command,
-                         fun(_NodeRef,  _Message) -> ok end),
-             application:set_env(pushy, heartbeat_interval, ?HB_INTERVAL),
+             basic_setup(),
              {ok, Pid} = ?NS:start_link(?NODE),
              {Pid}
      end,
      fun({Pid}) ->
-             meck:unload(pushy_command_switch),
+             basic_cleanup(),
              erlang:unlink(Pid),
              erlang:exit(Pid, kill),
              ok
@@ -88,57 +104,59 @@ heartbeat_test_() ->
       fun(_) ->
               {"Start it up, check that we can get state",
                fun() ->
-                       V = ?NS:current_state(?NODE),
-                       ?assertEqual(down, V)
+                       V = ?NS:status(?NODE),
+                       ?assertMatch({online,{unavailable, none}}, V)
                end}
       end,
       fun(_) ->
               {"Start it up, send hb",
                fun() ->
                        ?NS:heartbeat(?NODE),
-                       V = ?NS:current_state(?NODE),
-                       ?assertEqual(down, V)
+                       V = ?NS:status(?NODE),
+                       ?assertMatch({online,{unavailable, none}}, V)
                end}
       end,
       fun(_) ->
               {"Start it up, send hb, sleep, check state",
                fun() ->
                        ?NS:heartbeat(?NODE),
-                       V1= ?NS:current_state(?NODE),
-                       ?assertEqual(down, V1),
+                       V1= ?NS:status(?NODE),
+                       ?assertEqual({online, {unavailable, none}}, V1),
                        timer:sleep(?HB_INTERVAL),
-                       V2 = ?NS:current_state(?NODE),
-                       ?assertMatch(down, V2)
+                       V2 = ?NS:status(?NODE),
+                       ?assertMatch({online, {unavailable, none}}, V2)
                end}
       end,
       fun(_) ->
               {"Start it up, send hb, sleep, check state until we drive it into 'up'",
                fun() ->
                        ?NS:heartbeat(?NODE),
-                       V1 = ?NS:current_state(?NODE),
-                       ?assertEqual(down, V1),
+                       ?ASSERT_DOWN(?NODE),
                        timer:sleep(?HB_INTERVAL),
 
+                       %% Drive it up
                        heartbeat_step(?NODE, ?HB_INTERVAL,6),
-                       V2 = ?NS:current_state(?NODE),
-                       ?assertMatch(up, V2)
+                       ?NS:aborted(?NODE),
+                       ?ASSERT_UP(?NODE)
                end}
       end,
       fun(_) ->
               {"Start it up, send hb, sleep, check state until we drive it into 'up', then wait until it goes down",
                fun() ->
                        ?NS:heartbeat(?NODE),
-                       V1 = ?NS:current_state(?NODE),
-                       ?assertEqual(down, V1),
+                       ?ASSERT_DOWN(?NODE),
                        timer:sleep(?HB_INTERVAL),
 
+                       %% Drive it up
                        heartbeat_step(?NODE, ?HB_INTERVAL, 6),
-                       V2 = ?NS:current_state(?NODE),
-                       ?assertMatch(up, V2),
+                       ?NS:aborted(?NODE),
+                       ?ASSERT_UP(?NODE),
 
+                       %% Drive it down, scan, then wait a moment and check
                        timer:sleep(?HB_INTERVAL*7),
-                       V3 = ?NS:current_state(?NODE),
-                       ?assertMatch(down, V3)
+                       pushy_node_stats:scan(),
+                       timer:sleep(?HB_INTERVAL),
+                       ?ASSERT_DOWN(?NODE)
                end}
       end
      ]}.
@@ -146,16 +164,12 @@ heartbeat_test_() ->
 watcher_test_() ->
     {foreach,
      fun() ->
-             test_util:start_apps(),
-             meck:new(pushy_command_switch, []),
-             meck:expect(pushy_command_switch, send_command,
-                         fun(_NodeRef,  _Message) -> ok end),
-             application:set_env(pushy, heartbeat_interval, ?HB_INTERVAL),
+             basic_setup(),
              {ok, Pid} = ?NS:start_link(?NODE),
              {Pid}
      end,
      fun({Pid}) ->
-             meck:unload(pushy_command_switch),
+             basic_cleanup(),
              erlang:unlink(Pid),
              erlang:exit(Pid, kill),
              ok
@@ -163,24 +177,18 @@ watcher_test_() ->
      [fun(_) ->
               {"Enable watchpoint",
                fun() ->
-                       ?NS:start_watching(?NODE)
-               end}
-      end,
-      fun(_) ->
-              {"Disable watchpoint when none exists",
-               fun() ->
-                       ?NS:stop_watching(?NODE)
+                       ?NS:watch(?NODE)
                end}
       end,
       fun(_) ->
               {"Start it up, start watch, do hb, check that we don't get a message w/o state change",
                fun() ->
                        ?NS:heartbeat(?NODE),
-                       V1= ?NS:current_state(?NODE),
-                       ?assertEqual(down, V1),
+                       V1= ?NS:status(?NODE),
+                       ?assertEqual({online, {unavailable, none}}, V1),
                        timer:sleep(?HB_INTERVAL),
-                       V2 = ?NS:current_state(?NODE),
-                       ?assertMatch(down, V2),
+                       V2 = ?NS:status(?NODE),
+                       ?assertEqual({online, {unavailable, none}}, V2),
                        Msg = receive
                                  X -> X
                              after
@@ -192,66 +200,28 @@ watcher_test_() ->
       fun(_) ->
               {"Start it up, send hb, check state until we drive it into 'up'",
                fun() ->
-                       ?NS:start_watching(?NODE),
+                       ?NS:watch(?NODE),
                        ?NS:heartbeat(?NODE),
-                       V1 = ?NS:current_state(?NODE),
-                       ?assertEqual(down, V1),
-                       timer:sleep(?HB_INTERVAL),
-
                        heartbeat_step(?NODE, ?HB_INTERVAL, 6),
-                       V2 = ?NS:current_state(?NODE),
-                       ?assertMatch(up, V2)
+                       ?NS:aborted(?NODE),
+                       ?ASSERT_UP(?NODE)
                end}
       end,
       fun(_) ->
               {"Start it up, send hb, check state until we drive it into 'up', then wait until it goes down",
                fun() ->
-                       ?NS:start_watching(?NODE),
-                       ?NS:heartbeat(?NODE),
-                       V1 = ?NS:current_state(?NODE),
-                       ?assertEqual(down, V1),
-                       timer:sleep(?HB_INTERVAL),
-
+                       ?NS:watch(?NODE),
                        heartbeat_step(?NODE, ?HB_INTERVAL, 6),
-                       V2 = ?NS:current_state(?NODE),
-                       ?assertMatch(up, V2),
+                       ?NS:aborted(?NODE),
+                       ?ASSERT_UP(?NODE),
 
                        timer:sleep(?HB_INTERVAL*7),
-                       V3 = ?NS:current_state(?NODE),
-                       ?assertMatch(down, V3),
+                       ?ASSERT_DOWN(?NODE),
 
                        assertReceive({down, ?NODE})
                end}
       end
      ]}.
-
-logging_test_() ->
-    {foreach,
-     fun() ->
-             test_util:start_apps(),
-             application:set_env(pushy, heartbeat_interval, ?HB_INTERVAL),
-             {ok, Pid} = ?NS:start_link(?NODE),
-             {Pid}
-     end,
-     fun({Pid}) ->
-             erlang:unlink(Pid),
-             erlang:exit(Pid, kill),
-             ok
-     end,
-     [fun(_) ->
-              {"Enable logging",
-               fun() ->
-                       ?NS:set_logging(?NODE, verbose)
-               end}
-      end,
-      fun(_) ->
-              {"Disable logging",
-               fun() ->
-                       ?NS:set_logging(?NODE, normal)
-               end}
-      end
-     ]}.
-
 
 heartbeat_step(_Node, _SleepTime, 0) ->
     ok;
@@ -261,7 +231,7 @@ heartbeat_step(Node, SleepTime, Count) ->
 
 heartbeat_step(Node, SleepTime) ->
     ?NS:heartbeat(Node),
-    _V = ?NS:current_state(Node),
+    _V = ?NS:status(Node),
     timer:sleep(SleepTime).
 
 assertReceive(Expected) ->
@@ -271,5 +241,3 @@ assertReceive(Expected) ->
         100 -> none
     end,
     ?assertEqual(Expected, Got).
-
-
