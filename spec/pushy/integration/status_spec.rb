9b7953e34a0b5eabfda3ba062b120d7a0eb05983
doubt72 <doug@opscode.com>
2012-11-26 12:57:12 -0700

    Spec + lib files for pedant tests

---
+++ spec/pushy/integration/status_spec.rb
@@ -0,0 +1,60 @@
+#
+# -*- indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
+# ex: ts=4 sw=4 et
+#
+# Author:: Douglas Triggs (<doug@opscode.com>)
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+#
+
+describe "pushy status" do
+  describe 'access control' do
+    context 'GET /_status' do
+
+      # TODO: currently all tests are pending, because the _status endpoint can't
+      # be reached at all with the current combination of pushy and nginx routing
+
+      let(:path) { api_url("/_status").gsub(/organizations\/.*\//, '') }
+
+      it 'returns a 200 ("OK") for admin', :pending do
+        puts path
+        get(path, admin_user) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+
+      it 'returns a 200 ("OK") for normal user', :pending do
+        get(path, normal_user) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+
+      it 'returns a 200 ("OK") for client', :pending do
+        get(path, client) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+
+      it 'returns a 200 ("OK") for invalid user', :pending do
+        get(path, invalid_user) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+
+      it 'returns a 200 ("OK") for outside user', :pending do
+        get(path, outside_user) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+    end
+  end
+end # describe "pushy status"
---
+++ spec/pushy/spec_helper.rb
@@ -0,0 +1,18 @@
+$:.unshift File.expand_path("../../lib", __FILE__)
+$:.unshift File.expand_path("../..", __FILE__)
+
+require 'bundler'
+Bundler.require(:default, :test)
+
+#require 'pushy-client'
+
+require 'tmpdir'
+require 'tempfile'
+
+require 'support/end_to_end_util'
+
+WATCH = lambda { |x| puts x } unless defined?(WATCH)
+
+# Load everything from spec/support
+# Do not change the gsub.
+Dir["spec/support/**/*.rb"].map { |f| f.gsub(%r{.rb$}, '') }.each { |f| require f }
