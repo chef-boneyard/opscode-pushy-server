43cc8bd08b1f3b6728e4104f7f08ad15660e569f
Mark Anderson <mark@opscode.com>
2012-11-30 11:55:11 -0800

    Add support for addresses and lookup by address.

--- apps/pushy/src/pushy_node_state_sup.erl
+++ apps/pushy/src/pushy_node_state_sup.erl
@@ -13,7 +13,8 @@
 -export([start_link/0,
          get_or_create_process/1,
          get_process/1,
-         mk_gproc_name/1]).
+         mk_gproc_name/1,
+         mk_gproc_addr/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -45,8 +46,14 @@ get_or_create_process(NodeRef) ->
         Pid -> Pid
     end.
 
-get_process(NodeRef) ->
+get_process({_,_} =NodeRef) ->
     GprocName = mk_gproc_name(NodeRef),
+    get_process_int(GprocName);
+get_process(Addr) when is_binary(Addr) ->
+    GprocName = mk_gproc_addr(Addr),
+    get_process_int(GprocName).
+
+get_process_int(GprocName) ->
     case catch gproc:lookup_pid({n,l,GprocName}) of
         {'EXIT', _} ->
             undefined;
@@ -56,6 +63,10 @@ get_process(NodeRef) ->
 -spec mk_gproc_name(node_ref()) -> {'heartbeat', org_id(), node_name()}.
 mk_gproc_name({OrgId, NodeName}) when is_binary(OrgId) andalso is_binary(NodeName) ->
     {heartbeat, OrgId, NodeName}.
+
+-spec mk_gproc_addr(binary()) -> {'addr', binary()}.
+mk_gproc_addr(Addr) when is_binary(Addr) ->
+    {addr, Addr}.
 
 %% ------------------------------------------------------------------
 %% supervisor Function Definitions
