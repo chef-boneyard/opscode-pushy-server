1eec56fa6e27e1a4e8e550bb9358dbd827c99c68
doubt72 <doug@opscode.com>
2012-12-05 16:44:33 -0700

    Changes to config_state to store requestor

--- apps/pushy/include/pushy_wm.hrl
+++ apps/pushy/include/pushy_wm.hrl
@@ -12,7 +12,9 @@
           node_name :: string(),
           % TODO: can't use #pushy_job{} here without some minor re-jiggering elsewhere;
           % not everything that includes this includes pushy_sql.hrl
-          pushy_job :: tuple() }).
+          pushy_job :: tuple(),
+          requestor_id :: string(),
+          requestor_type :: atom() }).
 
 -define(AUTH_SKEW, 900).
 -define(MAX_SIZE, 1000000).
