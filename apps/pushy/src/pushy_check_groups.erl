56c60d94dd27780c47d5d88c38d54b2effefc9b2
doubt72 <doug@opscode.com>
2012-12-05 12:30:44 -0700

    Working group check

--- apps/pushy/src/pushy_check_groups.erl
+++ apps/pushy/src/pushy_check_groups.erl
@@ -9,40 +9,40 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -export([
-          group_membership/3
+          group_membership/4
         ]).
 
 %% @doc determine if a requestor is in a group, not in a group, or the group
 %% doesn't exist (returns undefined)
--spec group_membership(Name :: string(), Type :: string(),
-                             Group :: string()) -> true | false | group_not_found.
-group_membership(Name, Type, Group) ->
+-spec group_membership(Name :: string(), Type :: string(), OrgName :: string(),
+                       Group :: string()) -> true | false | group_not_found.
+group_membership(Name, Type, OrgName, Group) ->
     {ok, Key} = chef_keyring:get_key(pivotal),
-    Path = path(Group),
+    Path = path(OrgName, Group),
     Headers =  chef_authn:sign_request(Key, <<"">>, "pivotal",
                                        <<"GET">>, now,
                                        list_to_binary(Path)),
     FullHeaders = [{"Accept", "application/json"}|Headers],
-    group_membership(Name, Type, Group, FullHeaders).
+    group_membership(Name, Type, OrgName, Group, FullHeaders).
 
-group_membership(Name, Type, Group, Headers) ->
-    Url = url(Group),
+group_membership(Name, Type, OrgName, Group, Headers) ->
+    Url = url(OrgName, Group),
     case ibrowse:send_req(Url, Headers, get) of
         {ok, "404", _ResponseHeaders, _ResponseBody} ->
             group_not_found;
         {ok, Code, ResponseHeaders, ResponseBody} ->
             ok = check_http_response(Code, ResponseHeaders, ResponseBody),
-            parse_json_response(Name, Type, ResponseBody);
+            parse_json_response(Name, Type, OrgName, ResponseBody);
         {error, Reason} ->
             throw({error, Reason})
     end.
 
-url(Group) ->
+url(OrgName, Group) ->
     {ok, ErchefHost} = application:get_env(pushy, erchef_root_url),
-    ErchefHost ++ path(Group).
+    ErchefHost ++ path(OrgName, Group).
 
-path(Group) ->
-    "/groups/" ++ Group.
+path(OrgName, Group) ->
+    "/organizations/" ++ OrgName ++ "/groups/" ++ Group.
 
 %%
 %% Internal functions
@@ -50,7 +50,7 @@ path(Group) ->
 
 %% @doc determine if a member of the group, or recurse if necessary
 %%
-parse_json_response(Name, Type, Body) ->
+parse_json_response(Name, Type, OrgName, Body) ->
     try
         EJson = jiffy:decode(Body),
         Reqs = case Type of
@@ -59,12 +59,12 @@ parse_json_response(Name, Type, Body) ->
                    client ->
                        ej:get({<<"clients">>}, EJson)
                end,
-        case lists:member(Name, Reqs) of
+        case lists:member(list_to_binary(Name), Reqs) of
             true ->
                 true;
             false ->
                 Groups = ej:get({<<"groups">>}, EJson),
-                case check_subgroups(Name, Type, Groups) of
+                case check_subgroups(Name, Type, OrgName, Groups) of
                     true ->
                         true;
                     false ->
@@ -77,14 +77,14 @@ parse_json_response(Name, Type, Body) ->
     end.
 
 %% @doc recurse over subgroups
-check_subgroups(_, _, []) ->
+check_subgroups(_, _, _, []) ->
     false;
-check_subgroups(Name, Type, [Group|OtherGroups]) ->
-    case group_membership(Name, Type, Group) of
+check_subgroups(Name, Type, OrgName, [Group|OtherGroups]) ->
+    case group_membership(Name, Type, OrgName, Group) of
         true ->
             true;
         _ ->
-            check_subgroups(Name, Type, OtherGroups)
+            check_subgroups(Name, Type, OrgName, OtherGroups)
     end.
 
 %% @doc Check the code of the HTTP response and throw error if non-2XX
