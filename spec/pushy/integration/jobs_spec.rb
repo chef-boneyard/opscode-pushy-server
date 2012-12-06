15a7d27b03a21fb6019dfe9b3e9a802ce7dc0361
doubt72 <doug@opscode.com>
2012-12-06 10:27:21 -0700

    Added tests for nested pushy_job_reader/writer groups for jobs

--- spec/pushy/integration/jobs_spec.rb
+++ spec/pushy/integration/jobs_spec.rb
@@ -432,4 +432,225 @@ describe "Jobs API Endpoint", :focus, :jobs do
       end
     end # context 'GET /jobs/<name> with pushy_job_readers'
   end # describe 'access control with pushy_job groups'
+
+  describe 'access control with nested pushy_job groups' do
+    # Doing these in reverse for extra fun; this will guarantee it doesn't
+    # "accidentally" work if the groups are missing
+    let(:member) { normal_user }
+    let(:non_member) { admin_user }
+    let(:member_client) { platform.non_admin_client }
+    let(:non_member_client) { platform.admin_client }
+
+    let(:job_name) {
+      # This is evaluated at runtime, so there's always a (short-lived) job to
+      # detect during the test
+
+      post(api_url("/pushy/jobs"), member, :payload => payload)
+      get(api_url("/pushy/jobs"), member) do |response|
+        list = JSON.parse(response.body)
+        list[0]["id"]
+      end
+    }
+
+    let(:readers) { "pushy_job_readers" }
+    let(:writers) { "pushy_job_writers" }
+    let(:nested_readers) { "nested_pushy_job_readers" }
+    let(:nested_writers) { "nested_pushy_job_writers" }
+
+    before(:all) do
+      post(api_url("/groups/"), superuser,
+           :payload => { "groupname" => readers }) do |response|
+        response.should look_like({
+                                    :status => 201
+                                  })
+      end
+
+      post(api_url("/groups/"), superuser,
+           :payload => { "groupname" => nested_readers }) do |response|
+        response.should look_like({
+                                    :status => 201
+                                  })
+      end
+
+      put(api_url("/groups/#{readers}"), superuser,
+          :payload => { "groupname" => readers,
+            "actors" => { "groups" => [nested_readers] } } ) do |response|
+        response.should look_like({
+                                    :status => 200
+                                  })
+      end
+
+      put(api_url("/groups/#{nested_readers}"), superuser,
+          :payload => { "groupname" => nested_readers,
+            "actors" => { "users" => [member.name],
+              "clients" => [member_client.name] } } ) do |response|
+        response.should look_like({
+                                    :status => 200
+                                  })
+      end
+
+      post(api_url("/groups/"), superuser,
+           :payload => { "groupname" => writers }) do |response|
+        response.should look_like({
+                                    :status => 201
+                                  })
+      end
+
+      post(api_url("/groups/"), superuser,
+           :payload => { "groupname" => nested_writers }) do |response|
+        response.should look_like({
+                                    :status => 201
+                                  })
+      end
+
+      put(api_url("/groups/#{writers}"), superuser,
+          :payload => { "groupname" => writers,
+            "actors" => { "groups" => [nested_writers] } } ) do |response|
+        response.should look_like({
+                                    :status => 200
+                                  })
+      end
+
+      put(api_url("/groups/#{nested_writers}"), superuser,
+          :payload => { "groupname" => nested_writers,
+            "actors" => { "users" => [member.name],
+              "clients" => [member_client.name] } } ) do |response|
+        response.should look_like({
+                                    :status => 200
+                                  })
+      end
+    end
+
+    after(:all) do
+      delete(api_url("/groups/#{readers}"), superuser)
+      delete(api_url("/groups/#{writers}"), superuser)
+      delete(api_url("/groups/#{nested_readers}"), superuser)
+      delete(api_url("/groups/#{nested_writers}"), superuser)
+    end
+      
+    context 'GET /jobs with nested pushy_job_readers' do
+      it 'returns a 200 ("OK") for member' do
+        get(api_url("/pushy/jobs/"), member) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+
+      it 'returns a 403 ("Forbidden") for non-member' do
+        get(api_url("/pushy/jobs/"), non_member) do |response|
+          response.should look_like({
+                                      :status => 403,
+                                      :body_exact => {
+                                        "error" => non_member_authorization_failed_msg
+                                      }
+                                    })
+        end
+      end
+
+      it 'returns a 200 ("OK") for member client', :pending do
+        get(api_url("/pushy/jobs/"), member_client) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+
+      it 'returns a 403 ("Forbidden") for non-member client' do
+        get(api_url("/pushy/jobs/"), non_member_client) do |response|
+          response.
+            should look_like({
+                               :status => 403,
+                               :body_exact => {
+                                 "error" => non_member_client_authorization_failed_msg
+                               }
+                             })
+        end
+      end
+    end # context 'GET /jobs with nested pushy_job_readers'
+
+    context 'POST /jobs with nested pushy_job_writers' do
+      it 'returns a 200 ("OK") for member' do
+        post(api_url("/pushy/jobs/"), member, :payload => payload) do |response|
+          response.should look_like({
+                                      :status => 201
+                                    })
+        end
+      end
+
+      it 'returns a 403 ("Forbidden") for non-member' do
+        post(api_url("/pushy/jobs/"), non_member, :payload => payload) do |response|
+          response.should look_like({
+                                      :status => 403,
+                                      :body_exact => {
+                                        "error" => non_member_authorization_failed_msg
+                                      }
+                                    })
+        end
+      end
+
+      it 'returns a 200 ("OK") for member client', :pending do
+        post(api_url("/pushy/jobs/"), member_client,
+             :payload => payload) do |response|
+          response.should look_like({
+                                      :status => 201
+                                    })
+        end
+      end
+
+      it 'returns a 403 ("Forbidden") for non-member client' do
+        post(api_url("/pushy/jobs/"), non_member_client,
+             :payload => payload) do |response|
+          response.
+            should look_like({
+                               :status => 403,
+                               :body_exact => {
+                                 "error" => non_member_client_authorization_failed_msg
+                               }
+                             })
+        end
+      end
+    end # context 'POST /jobs with nested pushy_job_writers'
+
+    context 'GET /jobs/<name> with nested pushy_job_readers' do
+      it 'returns a 200 ("OK") for member' do
+        get(api_url("/pushy/jobs/#{job_name}"), member) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+
+      it 'returns a 403 ("OK") for non-member' do
+        get(api_url("/pushy/jobs/#{job_name}"), non_member) do |response|
+          response.should look_like({
+                                      :status => 403,
+                                      :body_exact => {
+                                        "error" => non_member_authorization_failed_msg
+                                      }
+                                    })
+        end
+      end
+
+      it 'returns a 200 ("OK") for member client', :pending do
+        get(api_url("/pushy/jobs/#{job_name}"), member_client) do |response|
+          response.should look_like({
+                                      :status => 200
+                                    })
+        end
+      end
+
+      it 'returns a 403 ("OK") for non-member client' do
+        get(api_url("/pushy/jobs/#{job_name}"), non_member_client) do |response|
+          response.
+            should look_like({
+                               :status => 403,
+                               :body_exact => {
+                                 "error" => non_member_client_authorization_failed_msg
+                               }
+                             })
+        end
+      end
+    end # context 'GET /jobs/<name> with nested pushy_job_readers'
+  end # describe 'access control with nested pushy_job groups'
 end # describe "Jobs API Endpoint"
