82a8daf0458cb6e3c07c4405208332e5f02e8a89
jamesc <james@opscode.com>
2012-12-13 15:36:29 -0800

    check_groups dialyzer - add pushy_requestor_type type

--- apps/pushy/include/pushy_wm.hrl
+++ apps/pushy/include/pushy_wm.hrl
@@ -3,19 +3,21 @@
 %% @author Douglas Triggs <doug@opscode.com>
 %% @copyright Copyright 2012 Opscode Inc.
 
--record(config_state, {
-          organization_name :: string(),
-          organization_guid :: string(),
-          % TODO: probably want to split this into specific states, instead of this
-          % catch-all, but right now authentication requires the above two things,
-          % and config + job endpoints (respectively) need the following:
-          node_name :: string(),
-          % TODO: can't use #pushy_job{} here without some minor re-jiggering elsewhere;
-          % not everything that includes this includes pushy_sql.hrl
-          pushy_job :: tuple(),
-          incarnation_id :: binary(),
-          requestor_id :: string(),
-          requestor_type :: atom() }).
+-type pushy_requestor_type() :: 'client' | 'user'.
+
+-record(config_state, {organization_name :: string(),
+                       organization_guid :: string(),
+                       % TODO: probably want to split this into specific states, instead of this
+                       % catch-all, but right now authentication requires the above two things,
+                       % and config + job endpoints (respectively) need the following:
+                       node_name :: string(),
+                       % TODO: can't use #pushy_job{} here without some minor re-jiggering elsewhere;
+                       % not everything that includes this includes pushy_sql.hrl
+                       pushy_job :: tuple(),
+                       incarnation_id :: binary(),
+                       requestor_id :: string(),
+                       requestor_type :: pushy_requestor_type()
+                      }).
 
 -define(AUTH_SKEW, 900).
 -define(MAX_SIZE, 1000000).
