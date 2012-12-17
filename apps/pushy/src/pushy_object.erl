bb9ab63106286f2f2d6ac19891d6fb086f5b73b9
jamesc <james@opscode.com>
2012-12-17 15:26:14 -0800

    pushy_object date_time and type fixes

--- apps/pushy/src/pushy_object.erl
+++ apps/pushy/src/pushy_object.erl
@@ -34,23 +34,30 @@ fetch_org_id(OrgName) ->
             Guid
     end.
 
+-spec new_record(atom(),
+                 OrgId :: object_id(),
+                 NodeNames :: [binary()],
+                 Command :: binary(),
+                 RunTimeout :: non_neg_integer(),
+                 Quorum :: non_neg_integer()) -> pushy_object().
 new_record(pushy_job, OrgId, NodeNames, Command, RunTimeout, Quorum) ->
     Id = make_org_prefix_id(OrgId),
+    Now = pushy_sql:sql_date(now),
     #pushy_job{id = Id,
                 org_id = OrgId,
                 status = new,
                 command = Command,
                 run_timeout = RunTimeout,
                 quorum = Quorum,
-                created_at = sql_date(now),
-                updated_at = sql_date(now),
+                created_at = Now,
+                updated_at = Now,
                 job_nodes = [
                   #pushy_job_node{job_id = Id,
                                   org_id = OrgId,
                                   node_name = NodeName,
                                   status = new,
-                                  created_at = sql_date(now),
-                                  updated_at = sql_date(now)} ||
+                                  created_at = Now,
+                                  updated_at = Now} ||
                                   NodeName <- NodeNames]
                 }.
 
@@ -85,11 +92,11 @@ update_object(Fun, Object) ->
 %% `set_updated' using standard emacs code tools.
 -define(MAKE_SET_UPDATED_WITH_ACTOR(Rec),
         set_updated(#Rec{}=Object, ActorId) ->
-            Object#Rec{updated_at = sql_date(now), last_updated_by = ActorId}).
+            Object#Rec{updated_at = pushy_sql:sql_date(now), last_updated_by = ActorId}).
 
 -define(MAKE_SET_UPDATED(Rec),
         set_updated(#Rec{}=Object) ->
-            Object#Rec{updated_at = sql_date(now)}).
+            Object#Rec{updated_at = pushy_sql:sql_date(now)}).
 
 ?MAKE_SET_UPDATED_WITH_ACTOR(pushy_job).
 
@@ -99,24 +106,11 @@ update_object(Fun, Object) ->
 %% created_at, updated_at, and last_updated_by on any of the Chef object types.
 -define(MAKE_SET_CREATED(Rec),
         set_created(#Rec{}=Object, ActorId) ->
-               Now = sql_date(now),
+               Now = pushy_sql:sql_date(now),
                Object#Rec{created_at = Now,
                           updated_at = Now, last_updated_by = ActorId}).
 
 ?MAKE_SET_CREATED(pushy_job).
-
-%% CHEF_COMMON CARGO_CULT
-%% chef_db:sql_date/1
-
-%%%
-%%% Emit in DATETIME friendly format
-%%% TODO: Modify to generate datetime pseudo record as used by emysql?
-sql_date(now) ->
-    sql_date(os:timestamp());
-sql_date({_,_,_} = TS) ->
-    {{Year,Month,Day},{Hour,Minute,Second}} = calendar:now_to_universal_time(TS),
-    iolist_to_binary(io_lib:format("~4w-~2..0w-~2..0w ~2..0w:~2..0w:~2..0w",
-                  [Year, Month, Day, Hour, Minute, Second])).
 
 %% CHEF_COMMON CARGO_CULT
 %% chef_db:make_org_prefix_id/1
