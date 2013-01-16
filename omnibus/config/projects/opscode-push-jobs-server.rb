8730c483a5015039fdfb15b56f68fbe741900519
jamesc <james@opscode.com>
2013-01-15 16:38:45 -0800

    Use semver nameing for pushy packages

--- omnibus/config/projects/opscode-push-jobs-server.rb
+++ omnibus/config/projects/opscode-push-jobs-server.rb
@@ -19,7 +19,7 @@ name "opscode-push-jobs-server"
 
 replaces        "opscode-push-jobs-server"
 install_path    "/opt/opscode-push-jobs-server"
-build_version   Omnibus::BuildVersion.full
+build_version Omnibus::BuildVersion.new.semver
 build_iteration "1"
 
 runtime_dependencies [ "private-chef" ]
