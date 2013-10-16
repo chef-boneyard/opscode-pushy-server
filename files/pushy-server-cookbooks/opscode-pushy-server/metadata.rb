fe2dc4aad86a9169007671f9c9e4bc26e3ba25b2
Christopher Maier <cm@opscode.com>
2013-10-16 16:59:18 -0400

    Use runit community cookbook
    Since we're using Berkshelf to move the cookbooks to their final
    location, we need to explicitly handle dropping off the solo.rb,
    show-config.json, and dna.json files. We now generate them, as we do
    for Enterprise Chef.

--- files/pushy-server-cookbooks/opscode-pushy-server/metadata.rb
+++ files/pushy-server-cookbooks/opscode-pushy-server/metadata.rb
@@ -1,7 +1,8 @@
+name             'opscode-pushy-server'
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures opscode-pushy-server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-depends          "runit"
+depends          'runit', '= 1.3.0'
