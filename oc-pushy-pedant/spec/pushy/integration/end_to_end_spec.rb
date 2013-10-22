6926dbee44bcb49d21f7ad8a46ca752fcdd4ba00
Christopher Maier <cm@opscode.com>
2013-10-22 06:49:12 -0400

    Run server-restarting tests only on backend master machines
    This configuration should be set as appropriate by Chef, according to the server topology being used.

--- oc-pushy-pedant/spec/pushy/integration/end_to_end_spec.rb
+++ oc-pushy-pedant/spec/pushy/integration/end_to_end_spec.rb
@@ -747,7 +747,11 @@ describe "end-to-end-test" do
       @long_job = start_job('sleep 20', [ 'DONKEY' ])
     end
 
-    context 'and the server goes down and comes back up' do
+    context 'and the server goes down and comes back up', :pending => (not (!!Pedant.config[:running_from_backend])) do
+      # This should only run on a maching that runs backend services
+      # because we currently shut down the pushy server by shelling
+      # out; this doesn't work on frontend machines because there's no
+      # service to shell out to.
       before :each do
         restart_server
         wait_for_server_restart
