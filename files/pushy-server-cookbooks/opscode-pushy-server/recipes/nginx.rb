d5eab795a2614d56b2b0d10af5032f9dbef798c4
Christopher Maier <cm@opscode.com>
2013-10-24 17:29:36 -0400

    Clean up the nginx recipe a bit
    We can just leverage the runit_service for nginx instead of creating
    our own one-off.
    
    We also just need to guard against a restart on the backend secondary
    machine, since nginx should be running on both the backend and
    frontend.

--- files/pushy-server-cookbooks/opscode-pushy-server/recipes/nginx.rb
+++ files/pushy-server-cookbooks/opscode-pushy-server/recipes/nginx.rb
@@ -9,6 +9,13 @@ nginx_dir = node['private_chef']['nginx']['dir']
 nginx_etc_dir = File.join(nginx_dir, "etc")
 nginx_addon_dir = File.join(nginx_etc_dir, "addon.d")
 
+# This will already exist, thanks to Enterprise Chef.  We simply
+# restate it here for notifying once we lay down some extra config
+# files.
+runit_service "nginx" do
+  action :nothing
+end
+
 # Pushy LB configs
 ["upstreams", "external"].each do |config|
   file = File.join(nginx_addon_dir, "10-push_jobs_#{config}.conf")
@@ -18,16 +25,6 @@ nginx_addon_dir = File.join(nginx_etc_dir, "addon.d")
     owner "root"
     group "root"
     mode "0644"
-    notifies :restart, 'service[nginx]' if OmnibusHelper.should_notify?("nginx",
-                                                                        node['pushy']['chef_base_path'],
-                                                                        "private-chef-ctl")
+    notifies :restart, 'runit_service[nginx]' unless backend_secondary?
   end
-end
-
-service "nginx" do
-    control_cmd = "#{node['pushy']['chef_base_path']}/embedded/bin/sv"
-    provider Chef::Provider::Service::Simple
-    supports :restart => true, :status => true
-    restart_command "#{control_cmd} restart nginx"
-    action :nothing
 end
