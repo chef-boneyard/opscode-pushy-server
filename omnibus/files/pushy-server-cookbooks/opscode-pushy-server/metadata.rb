e4b90d822caa33fe86a3a62f1ad7e7f9ccd1e82b
Christopher Maier <cm@opscode.com>
2013-10-25 15:08:15 -0400

    Bump enterprise cookbook to 0.3.0

--- omnibus/files/pushy-server-cookbooks/opscode-pushy-server/metadata.rb
+++ omnibus/files/pushy-server-cookbooks/opscode-pushy-server/metadata.rb
@@ -6,4 +6,4 @@ description      "Installs/Configures opscode-pushy-server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-depends          'enterprise', "= 0.2.0" # grabbed via Berkshelf + Git
+depends          'enterprise' # grabbed via Berkshelf + Git
