102cc7493be19bc1df8d32ccb6f391480d964c9b
Steven Grady <steven.grady@erlang-solutions.com>
2014-05-01 15:12:14 -0700

    Switch to new max_sockets API

--- apps/pushy/src/pushy_app.erl
+++ apps/pushy/src/pushy_app.erl
@@ -50,7 +50,7 @@ start(_StartType, _StartArgs) ->
             error_logger:info_msg("Starting Pushy incarnation ~s.~n", [IncarnationId]),
 
             IoProcesses = envy:get(pushy, zmq_io_processes, 1, integer),
-            case erlzmq:context(IoProcesses) of
+            case erlzmq:context(IoProcesses, [{max_sockets, 51200}]) of
                 {ok, Ctx} ->
                     case pushy_sup:start_link(#pushy_state{ctx=Ctx, incarnation_id=IncarnationId}) of
                         {ok, Pid} -> {ok, Pid, Ctx};
