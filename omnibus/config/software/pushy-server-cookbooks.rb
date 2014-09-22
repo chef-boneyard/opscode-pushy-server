d8932e9ff68946c66812fa47d44a700b3bc74d37
Yvonne Lam <yvonne@opscode.com>
2014-09-22 13:45:23 -0700

    Update pushy-server-cookbooks to Omnibus 4.

--- omnibus/config/software/pushy-server-cookbooks.rb
+++ omnibus/config/software/pushy-server-cookbooks.rb
@@ -1,18 +1,7 @@
 #
-# Copyright:: Copyright (c) 2012 Opscode, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2014 Chef Software, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# All Rights Reserved.
 #
 
 name "pushy-server-cookbooks"
@@ -22,28 +11,33 @@ dependency 'berkshelf2'
 cookbook_name = "opscode-pushy-server"
 cookbook_dir = "#{install_dir}/embedded/cookbooks"
 
-source :path => File.expand_path("files/pushy-server-cookbooks", Omnibus.project_root)
+source path: "#{project.files_path}/pushy-server-cookbooks"
 
 build do
-  command "mkdir -p #{cookbook_dir}"
-  command "cd #{cookbook_name} && #{install_dir}/embedded/bin/berks install --berksfile=./Berksfile --path=#{cookbook_dir}",
-          :env => { "RUBYOPT"         => nil,
-                    "BUNDLE_BIN_PATH" => nil,
-                    "BUNDLE_GEMFILE"  => nil,
-                    "GEM_PATH"        => nil,
-                    "GEM_HOME"        => nil } # Manual "bundler buster" needed, because #command doesn't do it for us
+  mkdir "#{cookbook_dir}"
+  command "cd #{cookbook_name} && #{install_dir}/embedded/bin/berks install" \
+          " --path=#{cookbook_dir}"
   block do
     File.open("#{cookbook_dir}/dna.json", "w") do |f|
-      f.puts "{\"run_list\": [ \"recipe[#{cookbook_name}]\" ]}"
+      f.write JSON.fast_generate(
+        run_list: [
+          'recipe[opscode-pushy-server::default]',
+        ]
+      )
     end
     File.open("#{cookbook_dir}/show-config.json", "w") do |f|
-      f.puts "{\"run_list\": [ \"recipe[#{cookbook_name}::show_config]\" ]}"
+      f.write JSON.fast_generate(
+        run_list: [
+          'recipe[opscode-pushy-server::show_config]',
+        ]
+      )
     end
     File.open("#{cookbook_dir}/solo.rb", "w") do |f|
-      f.puts "CURRENT_PATH = File.expand_path(File.dirname(__FILE__))"
-      f.puts "file_cache_path \"\#\{CURRENT_PATH\}/cache\""
-      f.puts "cookbook_path CURRENT_PATH"
-      f.puts "verbose_logging true"
+      f.write <<-EOH.gsub(/^ {8}/, '')
+        cookbook_path   "#{install_dir}/embedded/cookbooks"
+        file_cache_path "#{install_dir}/embedded/cookbooks/cache"
+        verbose_logging true
+      EOH
     end
   end
 end
