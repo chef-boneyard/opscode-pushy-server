fc61bbc88357704b25a1fe597decdc23af7d7037
Yvonne Lam <yvonne@opscode.com>
2014-09-22 13:44:04 -0700

    Update oc-pushy-pedant to Omnibus 4.

--- omnibus/config/software/oc-pushy-pedant.rb
+++ omnibus/config/software/oc-pushy-pedant.rb
@@ -1,18 +1,7 @@
 #
-# Copyright:: Copyright (c) 2012 Opscode, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2014 Chef Software, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# All Rights Reserved.
 #
 
 name "oc-pushy-pedant"
@@ -21,15 +10,14 @@ default_version "1.0.6"
 dependency "libzmq"
 dependency "ruby"
 dependency "bundler"
-dependency "rsync"
 
 # TODO: use the public git:// uri once this repo is public
-source :git => "git@github.com:opscode/oc-pushy-pedant"
+source git: "git@github.com:opscode/oc-pushy-pedant"
 
 relative_path "oc-pushy-pedant"
 
 build do
-  bundle "install --path=#{install_dir}/embedded/service/gem"
-  command "mkdir -p #{install_dir}/embedded/service/oc-pushy-pedant"
-  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/oc-pushy-pedant/"
+  bundle "install" \
+         " --path=#{install_dir}/embedded/service/gem"
+  sync "#{project_dir}", "#{install_dir}/embedded/service/oc-pushy-pedant/"
 end
