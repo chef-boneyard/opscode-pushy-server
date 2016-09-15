cec98a8b17fe53636d1b60db546ff417df69a4f0
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-09-15 15:25:10 -0400

    Berkshelf and Ohai need "v" in their version tags.

--- omnibus/config/projects/opscode-push-jobs-server.rb
+++ omnibus/config/projects/opscode-push-jobs-server.rb
@@ -26,9 +26,9 @@ build_version   Omnibus::BuildVersion.new.semver
 build_iteration 1
 
 override :libzmq, version: "4.0.5"
-override :berkshelf, version: "4.3.5"
+override :berkshelf, version: "v4.3.5"
 override :chef, version: "12.12.15"
-override :ohai, version: "8.17.1"
+override :ohai, version: "v8.17.1"
 override :ruby, version: "2.1.8"
 override :rubygems, version: "2.6.6"
 override :bundler, version: "1.12.5"
