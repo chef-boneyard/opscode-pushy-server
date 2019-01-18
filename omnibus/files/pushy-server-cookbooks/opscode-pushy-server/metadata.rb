cabf676dc59a3896b758da3321db784a4cbe829c
Jaymala Sinha <jsinha@chef.io>
2019-01-18 11:43:51 -0500

    Update chef gem in pushy-server cookbook
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

--- omnibus/files/pushy-server-cookbooks/opscode-pushy-server/metadata.rb
+++ omnibus/files/pushy-server-cookbooks/opscode-pushy-server/metadata.rb
@@ -1,9 +1,9 @@
 name             'opscode-pushy-server'
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+maintainer       'Chef Software Inc Engineering'
+maintainer_email 'engineering@chef.io'
 license          "Apache 2.0"
 description      "Installs/Configures opscode-pushy-server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends          'enterprise' # grabbed via Berkshelf + Git
