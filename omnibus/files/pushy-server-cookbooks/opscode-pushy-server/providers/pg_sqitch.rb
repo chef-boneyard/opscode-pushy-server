a0e1c46d98b4703fb954e58fcb83470487bad288
Zane Geiger <zane.geiger@relativity.com>
2019-03-04 15:35:09 -0600

    Add sslmode property to pg_sqitch resource
    Signed-off-by: Zane Geiger <zane.geiger@relativity.com>

--- omnibus/files/pushy-server-cookbooks/opscode-pushy-server/providers/pg_sqitch.rb
+++ omnibus/files/pushy-server-cookbooks/opscode-pushy-server/providers/pg_sqitch.rb
@@ -20,7 +20,7 @@ action :deploy do
       EOM
       environment "PERL5LIB" => "", # force us to use omnibus perl
                   "PGPASSWORD" => new_resource.password,
-                  "PGSSLMODE" => node['pushy']['postgresql']['sslmode']
+                  "PGSSLMODE" => new_resource.sslmode
 
       # Sqitch Return Codes
       # 0 - when changes are applied
