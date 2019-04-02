af775eb3f94afd9b75108f70d91912bae512d54a
Jaymala Sinha <jsinha@chef.io>
2019-04-02 13:42:29 -0400

    Migrate verification tests to Buildkite
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

---
+++ scripts/bk_tests/bk_install.sh
@@ -0,0 +1,9 @@
+apt-get --purge remove -y postgresql-9.3
+apt-get update -y && apt-get install -y postgresql-9.6
+cp /workdir/scripts/bk_tests/pb_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
+asdf local erlang 18.3
+asdf local ruby 2.5.5
+gem install bundler --version '~> 1.17' --no-document
+cpanm --notest --quiet --local-lib=$HOME/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+cpanm --notest --quiet App::Sqitch
+service postgresql restart
