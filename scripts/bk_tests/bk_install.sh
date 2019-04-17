43e875060ab5dd1b287a51bf51c8fa743bd44589
Jaymala Sinha <jsinha@chef.io>
2019-04-17 15:44:08 -0400

    Use python2 for verification tests
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

--- scripts/bk_tests/bk_install.sh
+++ scripts/bk_tests/bk_install.sh
@@ -1,6 +1,8 @@
 apt-get --purge remove -y postgresql-9.3
-apt-get update -y && apt-get install -y postgresql-9.6
+apt-get update -y && apt-get install -y postgresql-9.6 python2.7
 cp /workdir/scripts/bk_tests/pb_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
+rm /bin/python
+ln -s /usr/bin/python2.7 /bin/python
 asdf local erlang 18.3
 asdf local ruby 2.5.5
 gem install bundler --version '~> 1.17' --no-document
