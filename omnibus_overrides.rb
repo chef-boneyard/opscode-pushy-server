c09ee0c6bf99588212f55b05076be0b478d62573
Faizan <60912283+FaizanSRE@users.noreply.github.com>
2020-04-30 17:00:02 +0530

    change ruby version

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -1,5 +1,5 @@
 override :libzmq, version: "4.0.5"
 override :chef, version: "v15.10.12"
 override :ohai, version: "v15.9.1"
-override :ruby, version: "2.6.6"
+override :ruby, version: "2.5.5"
 override "berkshelf-no-depselector".to_sym, version: "2cde3eeac0ee2cb45d07630f124080e6e6762aee"
