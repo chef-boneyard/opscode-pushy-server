b646e656cbd68acdfcf819a3047e9842d75e8b93
Bryan McLellan <btm@loftninjas.org>
2020-05-11 19:29:28 -0400

    Try major upgrade to lastest version of the pg gem
    Signed-off-by: Bryan McLellan <btm@loftninjas.org>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -2,5 +2,6 @@ override :libzmq, version: "4.0.5"
 override :chef, version: "v15.10.12"
 override :ohai, version: "v15.9.1"
 override :ruby, version: "2.6.6"
+override :'pg-gem', version: "1.2.3"
 # TMP
 #override "berkshelf-no-depselector".to_sym, version: "2cde3eeac0ee2cb45d07630f124080e6e6762aee"
